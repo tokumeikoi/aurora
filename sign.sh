b747eb754cf4df9525cfaacf96edd85dfe123345
root <root@root.com>
2020-07-02 21:47:30 +0800

    update

--- sign.sh
+++ sign.sh
@@ -23,8 +23,9 @@ chmod 755 *
 if ls ./.lego/certificates | grep "$domain"
     then
     echo '证书签发成功'
-    cp ./.lego/certificates/v2ray.crt /home
-    cp ./.lego/certificates/v2ray.key /home
+    mkdir /root/.cert
+    cp ./.lego/certificates/$domain.crt /root/.cert/server.crt
+    cp ./.lego/certificates/$domain.key /root/.cert/server.key
 else
     echo '证书签发失败'
 fi
